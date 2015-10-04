450a0c5e62425cd1b8fd26447492bba85bd85b5c
VsevolodTrofimov <seva.trofimov2@gmail.com>
2015-10-04 22:57:09 +0300

    0.0.5
    Небольшие изменения в фронтенде
    gulp теперь вырезает элементы с аттрибутом, dev,
    пользутесь
    
    Частично готов drag'n'drop

--- Frontend/gulpfile.js
+++ Frontend/gulpfile.js
@@ -1,16 +1,20 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var cleancss = require('gulp-cleancss');
-var autoprefixer = require('gulp-autoprefixer');
-var uglify = require('gulp-uglify');
-// var inline = require('gulp-inline')
-var inlineSource = require('gulp-inline-source');
-var htmlmin = require('gulp-htmlmin');
-// var imagemin = require('gulp-imagemin');
+	//things sass & css need
+var	sass = require('gulp-sass'); //compiles sass into css
+var	cleancss = require('gulp-cleancss'); //makes css lighter
+var	autoprefixer = require('gulp-autoprefixer'); //sets vendor prefixes in
+	//things js needs
+var	uglify = require('gulp-uglify'); //makes js lighter
+	//things html needs
+var	htmlmin = require('gulp-htmlmin'); //makes html lighter
+var	inlineSource = require('gulp-inline-source'); //inlines svg,css,js
+var	remove = require('gulp-html-remove'); //removes things you don't want from html
+// var imagemin = require('gulp-imagemin'); //makes images lighter
 
 gulp.task('sass_to_css', function () {
+	//converts sass to css, prefixes, and minificates css
 	gulp.src('./Styles/Sass/*.sass')
 		.pipe(sass({outputStyle: 'compressed'}))
 		.pipe(autoprefixer({
@@ -21,26 +25,30 @@ gulp.task('sass_to_css', function () {
 });
 
 gulp.task('build', function () {
-
+	//minificates, inlines and removes useless shit
 	gulp.src('HTML/*.html')
 		.pipe(inlineSource())
+		.pipe(remove("[dev]"))		//removes elements with attr dev
 		.pipe(htmlmin({collapseWhitespace: true}))
 	 	.pipe(gulp.dest('./Build'));
 });
 
 gulp.task('test_build', function () {
+	//build with no minification and other user-needs dev-hates stuff
 	gulp.src('HTML/*.html')
 		.pipe(inlineSource())
 	 	.pipe(gulp.dest('./Test'));
 });
 
 gulp.task('minjs', function () {
+	//minifcates js
 	gulp.src('./Scripts/Dev/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('./Scripts/Min/'));
 });
 
 /*gulp.task('minpic', function () {
+	//minificates pics
 	gulp.src('./Assets/Images/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest("./Build/Assets/Images"));
