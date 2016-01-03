2e4cf10c67a518939ea739b96f0837f5208578b1
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-01-03 17:03:35 +0300

    0.0.48
    Ripple effect added

---
+++ Frontend/Scripts/Dev/ripple.js
@@ -0,0 +1,39 @@
+var ripple = $("<div class='ripple'></div>");
+ripple.dissolve = function (){
+	ripple.css({
+		"transform": "scale(0)",
+		"opacity": 0,
+		"transition-duration": "1000s"
+	});
+}
+$(document).ready(function() {
+	$(".link--card>.card, .link--card>.card--small, button").bind({
+		mousedown: function(e) {
+			$(this).prepend(ripple);
+			var c_rect = this.getBoundingClientRect();
+			var scale = (c_rect.width/5)*1.4;
+			console.log(scale);
+			var time = scale * 6;
+			console.log(this.getBoundingClientRect(), e, $("main").scrollTop());
+			var pos = {
+				x: e.clientX - c_rect.left,
+				y: e.clientY - c_rect.top - $("main").scrollTop()
+			}
+			console.log(pos);
+			ripple.css({
+				"top": pos.y + "px",
+				"left": pos.x + "px",
+				"transform": "scale(" + scale + ")",
+				"transition-duration": time + "ms",
+				"opacity": 1
+			});
+		},
+
+		mouseup: function() {
+			ripple.dissolve();
+		},
+		mouseleave: function() {
+			ripple.dissolve();
+		},
+	});
+});
