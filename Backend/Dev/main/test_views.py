bcecdfd5192b679677468dd7ce8607dba6a167c3
Gollut <artem200024@rambler.ru>
2016-03-21 12:09:44 +0300

    0.0.154
    Tonns of bugs fixed, test_editor backend fixed (changed), added backend
    for creating assignment

--- Backend/Dev/main/test_views.py
+++ Backend/Dev/main/test_views.py
@@ -5,6 +5,7 @@ from .models import User, Course
 # from django.core.servers.basehttp import FileWrapper
 import os
 import json
+import io
 
 def edit(request):
 	#switch for create\load test, lauches test editor anyway
@@ -19,12 +20,10 @@ def edit(request):
 def create(request):
 	#creates test environment (no test exists as file untill saved)
 	course_id =request.GET["course_id"]
-	
-	info_file = open('courses/'+course_id+'/info.json', 'r')
-	course_info = json.loads(info_file.read())
-	test_id = str(course_info['tests']['amount']+1)
-	info_file.close()
-	
+	with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as data_file:
+		course_info = json.load(data_file)
+		print("1234")
+		test_id = str(course_info['tests']['amount']+1)
 	course = {"id": course_id}
 	test = {"id": test_id, "loaded": 0}
 	context =  {"test": test, "course": course}
@@ -35,64 +34,61 @@ def delete(request):
 	#moves test to trash bin
 	course_id =request.POST["course_id"]
 	test_id =request.POST["test_id"]
-	info_file = open('courses/'+course_id+'/info.json', 'r')
-	course_info = json.loads(info_file.read())
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as info_file:
+		course_info = json.load(info_file)
 
-	if test_id in course_info['tests']['published']:
-		course_info['tests']['published'].remove(test_id)
+		if test_id in course_info['tests']['published']:
+			course_info['tests']['published'].remove(test_id)
 
-	if test_id in course_info['tests']['unpublished']:
-		course_info['tests']['unpublished'].remove(test_id)
+		if test_id in course_info['tests']['unpublished']:
+			course_info['tests']['unpublished'].remove(test_id)
 
-	info_file = open('courses/'+course_id+'/info.json', 'w+')
-	info_file.write(json.dumps(course_info, ensure_ascii=False))
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'w+', encoding='utf8') as info_file:
+
+		info_file.write(json.dumps(course_info, ensure_ascii=False))
+
 	return HttpResponse("Тест удален")
 
 
 
 def save(request):
 	#saves test file
+	print("save")
 	if request.method == 'POST':
 		json_file = request.POST["json_file"]
 		course_id = request.POST["course_id"]
 		test_id = request.POST["test_id"]
 		json_file_path = 'courses/'+course_id+'/Tests/'+test_id+'.json'
 		if not os.path.isfile(json_file_path):
-			info_file = open('courses/'+course_id+'/info.json', 'r')
-			course_info = json.loads(info_file.read())
-			course_info['tests']['amount']+=1
-			course_info['tests']['unpublished'].append(test_id)
-			test_id = str(course_info['tests']['amount'])
-			info_file.close()
+			with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as info_file:
+				course_info = json.load(info_file)
+				course_info['tests']['amount']+=1
+				course_info['tests']['unpublished'].append(test_id)
+				test_id = str(course_info['tests']['amount'])
 
-			info_file = open('courses/'+course_id+'/info.json', 'w+')
-			info_file.write(json.dumps(course_info, ensure_ascii=False))
-			info_file.close()
-		test_file = open(json_file_path, 'w')
-		test_file.write(json_file)
+			with io.open('courses/'+course_id+'/info.json', 'w+', encoding='utf8') as info_file:
+				info_file.write(json.dumps(course_info, ensure_ascii=False))
+
+		with io.open(json_file_path, 'w', encoding='utf8') as test_file:
+			test_file.write(json_file)
 	return HttpResponse("Тест сохранен")
 
 def load(request):
 	#loads test file
 	course_id =request.GET["course_id"]
 	test_id =  request.GET["test_id"]
-	json_file = open('courses/'+course_id+'/Tests/'+test_id+'.json', 'r')
-	json_file = json_file.read()
+	with io.open('courses/'+course_id+'/Tests/'+test_id+'.json', 'r', encoding='utf8') as json_file:
 
-	info_file = open('courses/'+course_id+'/info.json', 'r')
-	course_info = json.loads(info_file.read())
-	info_file.close()
+		with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as info_file:
+			course_info = json.load(info_file)
 
-	course = {"id": course_id}
-	test = {
-		"id": test_id,
-		"loaded": 1,
-		"json": json_file,
-		"published" : test_id in course_info["tests"]["published"]
-	}
-
+		course = {"id": course_id}
+		test = {
+			"id": test_id,
+			"loaded": 1,
+			"json": json_file,
+			"published" : test_id in course_info["tests"]["published"]
+		}
 	context =  {"test": test, "course": course}
 	return render(request, 'Pages/test_editor.html', context)
 
@@ -102,17 +98,16 @@ def publish(request):
 	#makes test visible in course screen
 	course_id =request.POST["course_id"]
 	test_id =request.POST["test_id"]
-	info_file = open('courses/'+course_id+'/info.json', 'r')
-	course_info = json.loads(info_file.read())
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as info_file:
+		course_info = json.load(info_file)
+
 	if test_id in course_info['tests']['unpublished']:
 		course_info['tests']['unpublished'].remove(test_id)
 
 	course_info['tests']['published'].append(test_id)
 
-	info_file = open('courses/'+course_id+'/info.json', 'w+')
-	info_file.write(json.dumps(course_info, ensure_ascii=False))
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'w+', encoding='utf8') as info_file:
+		info_file.write(json.dumps(course_info, ensure_ascii=False))
 
 	return HttpResponse("Тест опубликован")
 
@@ -120,17 +115,16 @@ def unpublish(request):
 	#makes test invisible in course screen
 	course_id =request.POST["course_id"]
 	test_id =request.POST["test_id"]
-	info_file = open('courses/'+course_id+'/info.json', 'r')
-	course_info = json.loads(info_file.read())
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'r', encoding='utf8') as info_file:
+		course_info = json.load(info_file)
 	if test_id in course_info['tests']['published']:
 		course_info['tests']['published'].remove(test_id)
 
 	course_info['tests']['unpublished'].append(test_id)
 
-	info_file = open('courses/'+course_id+'/info.json', 'w+')
-	info_file.write(json.dumps(course_info, ensure_ascii=False))
-	info_file.close()
+	with io.open('courses/'+course_id+'/info.json', 'w+', encoding='utf8') as info_file:
+		info_file.write(json.dumps(course_info, ensure_ascii=False))
+
 	return HttpResponse("Тест скрыт")
 
 def share(request):
@@ -181,7 +175,7 @@ def upload_downloadable(request):
 	return HttpResponse("ok")
 
 #embmend
-def uplaod_embmend(request):
+def upload_embmend(request):
 	if request.method == 'POST':
 		asset = request.POST["code"]
 		course_id = request.POST["course_id"]
