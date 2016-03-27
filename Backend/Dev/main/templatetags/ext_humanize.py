732f5b6775f5a2a5d6c06bde2d6d5e76a0b6e41a
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-03-27 20:21:34 +0300

    0.0.166
    Issue #75 closed
    ---
    Avatar in card person resized to 2.25rem form 3rem

--- Backend/Dev/main/templatetags/ext_humanize.py
+++ Backend/Dev/main/templatetags/ext_humanize.py
@@ -20,12 +20,37 @@ def humanize_date(value):
     }
     day = value.split(".")[0]
     month = month_sting[int(value.split(".")[1])]
-    year = value.split(".")[2]
+    if len(value.split(".")) > 2:
+        year = value.split(".")[2]
     return day + " " + month + " " + year
 
 def of(value, max_value):
     percent = round((int(value)/max_value)*100)
     return str(percent)+"%<span>("+str(value)+" из "+str(max_value)+")</span>";
 
+
+def form_for_num(num, forms):
+    forms = forms.split(" ")
+    
+    belongs_1 = forms[0]
+    belongs_2_4 = forms[1]
+    many = forms[2]
+
+    form = ""
+    if num == 0:
+        return many+" нет"
+    elif num < 10 or str(num)[-2] != "1":
+        if num%10 == 0:
+            form = many
+        elif num%10 == 1:
+            form = belongs_1
+        elif num%10 <= 4:
+            form = belongs_2_4
+        else:
+            form = many
+    else:
+        form = many
+    return str(num) + " " + form
 register.filter('of', of)
 register.filter('humanize_date', humanize_date)
+register.filter('form_for_num', form_for_num)
