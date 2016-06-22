db110045c9dc48a103a99929fe10b9eff283c3b3
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-06-22 10:57:18 +0300

    Default state

---
+++ App/main/templates/Pages/Test/Attempt/results/_scripts/summary.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	var summary_template = function(index) {
+		return '<div class="card --small sumfor_'
+			+ index + ' row">'
+			+'<button class="__number">' + index + ':</button>'
+			+'<div class="__value">Пусто</div></div>';
+	}
+
+	panel.show("");
+
+	for
+}
