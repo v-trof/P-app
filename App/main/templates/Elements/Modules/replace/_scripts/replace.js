bdc25ed44f0a55bd83272ce4a7e160573a01dfd0
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-07-19 23:29:39 +0300

    0.0.305+
    Replace module fallback option

--- App/main/templates/Elements/Modules/replace/_scripts/replace.js
+++ App/main/templates/Elements/Modules/replace/_scripts/replace.js
@@ -28,3 +28,24 @@ $.fn.extend({
 		});
 	}
 });
+
+
+(function (a) {
+    a.fn.replaceTagName = function (f) {
+        var g = [],
+            h = this.length;
+        while (h--) {
+            var k = document.createElement(f),
+                b = this[h],
+                d = b.attributes;
+            for (var c = d.length - 1; c >= 0; c--) {
+                var j = d[c];
+                k.setAttribute(j.name, j.value)
+            }
+            k.innerHTML = b.innerHTML;
+            a(b).after(k).remove();
+            g[h - 1] = k
+        }
+        return a(g)
+    }
+})(window.jQuery);
