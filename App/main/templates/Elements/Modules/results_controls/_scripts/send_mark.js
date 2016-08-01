d6ff0453c0141fb0b1f65b0885d77d92850d9385
Gollut <artem200024@rambler.ru>
2016-08-01 13:38:54 +0300

    0.0.336+

--- App/main/templates/Elements/Modules/results_controls/_scripts/send_mark.js
+++ App/main/templates/Elements/Modules/results_controls/_scripts/send_mark.js
@@ -1,12 +1,13 @@
 results_controls.send_mark = function(index, mark) {
   $.ajax({
-    url: 'test/change_score/',
+    url: '/test/change_score/',
+    type: 'POST',
     data: {
       'csrfmiddlewaretoken': '{{ csrf_token }}',
       'course_id': "{{course.id}}",
       'test_id': results_controls.active_test,
-      'user_id': results_controls.active_user,
-      'question_id': index,
+      'user_id': results_controls.active_student,
+      'answer_id': index,
       'score': mark
     },
   })
