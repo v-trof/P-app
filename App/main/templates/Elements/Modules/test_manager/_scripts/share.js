f46df1feadd379f7073f4ba846da477d13cbff6f
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-09-06 14:00:22 +0600

    0.1.16
    Added share UI for test editor

---
+++ App/main/templates/Elements/Modules/test_manager/_scripts/share.js
@@ -0,0 +1,13 @@
+test_manager.share = function() {
+  if (test_manager.verify_test()){
+    popup.show('{% include "Pages/Test/editor/_popup_texts/share/exports.html" %}', function() {
+
+      inline_editor.start($('.popup--share .__description')[0]);
+      $("#share").click(function() {
+        //share ajax
+        $("#{{type}}_share").hide();
+        $("#{{type}}_unshare").show();
+      });
+    }); 
+  }
+}
