7786fa965ca7a5b4ad6c9985d2780ad441607893
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2016-12-21 23:18:59 +0300

    UI scripts added

---
+++ App/main/files/static/Pages/promo/_scripts/bind_scroll.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  scroll.wire($('.features-link'), $('.functions'));
+});
--- App/main/files/static/Pages/promo/_scripts/remake_header.js
+++ App/main/files/static/Pages/promo/_scripts/remake_header.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  var $start = $('<a>Pileus</a>');
-  var $funcs = $('<a>Возможноксти</a>');
-  var $contact = $('<a>Контакты</a>');
+  var $start = $('<span>Pileus</span>');
+  var $funcs = $('<span>Возможности</span>');
+  var $contact = $('<span>Контакты</span>');
+
+  $start.click(function() {
+    scroll.wire($start, $('.above-the-flood'));
+    scroll.wire($funcs, $('.functions'));
+    scroll.wire($contact, $('.contacts'));
+  });
+
+  $start.click();
 
   $('.__search').hide();
   $('.__breadcrumbs').html('').append([$start, $funcs, $contact]);
