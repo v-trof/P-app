6e08dfe2bc9a57c6a4bb4df18bd717e063e5c3ac
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2017-01-06 15:15:34 +0300

    0.3.42
    More modules are back

---
+++ App/main/files/static/Elements/Modules/Page_edit/toggle.js
@@ -0,0 +1,40 @@
+var edit = (function(exports) {
+  exports = {
+    editing: false,
+    start: function() {},
+    end: function() {},
+    save_changes: function() {},
+  }
+  return exports;
+})();
+
+$(document).ready(function() {
+  $linkbox = $(".linkbox").last();
+
+  $linkbox.append('<a class="m--card">' +
+    '<button class="m--flat" id="edit_toggle">Редактировать' +
+    '</button></a>');
+  $linkbox.append('<a class="m--card">' +
+    '<button class="m--flat" id="edit_discard">Отмена' +
+    '</button></a>');
+  $("#edit_discard").hide()
+
+  $("#edit_toggle").click(function(event) {
+    if (edit.editing) {
+      edit.editing = false;
+      edit.end();
+      $("#edit_toggle").text("Редактировать");
+      $("#edit_discard").hide();
+    } else {
+      edit.editing = true;
+      edit.start();
+      $("#edit_toggle").text("Сохранить изменения")
+      $("#edit_discard").show();
+
+    }
+  });
+
+  $("#edit_discard").click(function(event) {
+    location.reload();
+  });
+});
