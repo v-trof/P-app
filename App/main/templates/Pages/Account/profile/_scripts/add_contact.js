3d68a2cf7c4900e48099bc6332b790ea97357a49
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2017-01-10 23:23:45 +0300

    0.3.48
    Closes #281

--- App/main/templates/Pages/Account/profile/_scripts/add_contact.js
+++ App/main/templates/Pages/Account/profile/_scripts/add_contact.js
@@ -1,46 +1,49 @@
 contact_template = '{% include "Pages/Account/profile/contacts/item/exports.html" %}';
 
 $(document).ready(function() {
-
   $('#add_contact').click(function(e) {
     popup.show('{% include "Pages/Account/profile/_popup_texts/new_contact/exports.html" %}',
     function() {
       popup.$.find(".__submit").click(function(e) {
-        $new_contact = $(contact_template);
-        $new_contact.find(".__type")
-          .text(popup.$.find(".__contact-type").val());
+        var err = false;
+        var type = popup.$.find(".__contact-type").val();
+        var value = popup.$.find(".__contact-value").val();
 
-        $new_contact.find(".__value")
-          .text(popup.$.find(".__contact-value").val());
+        var $new_contact = $(contact_template);
+        $new_contact.find(".__type").text(type);
+        $new_contact.find(".__value").text(value);
 
-        if(popup.$.find(".__contact-value").val().length == 0) {
-          tooltip.show(popup.$.find(".__contact-value"),
-           "Введите контактную информацию");
-          popup.$.find(".__contact-value").keydown(function(event) {
-            tooltip.hide();
-          });
-        } else {
-          $.ajax({
-            type: "POST",
-            url: "/func/create_contact/",
-            data: {
-              'contact_type': popup.$.find(".__contact-type").val(),
-              'csrfmiddlewaretoken': '{{ csrf_token }}',
-              'contact_info': popup.$.find(".__contact-value").val()
-            },
-            success: function(response) {
-              if(response && response["type"]) {
-                notification.show(response["type"], response["message"]);
-              }  else {
-                notification.show('success','Контакт добавлен' );
-              }              
-              location.reload();
-            }
-          });
-          $("#contacts").append($new_contact);
-          popup.hide();  
+        console.log(type, ':', value);
+        if(value.length === 0) {
+          notification.show('warning', "Введите контактную информацию");
+          err = true;
         }
 
+        if(type.length === 0) {
+          notification.show('warning', "Выберите тип контакта");
+          err = true;
+        }
+
+        if(err) return;
+        $.ajax({
+          type: "POST",
+          url: "/func/create_contact/",
+          data: {
+            'contact_type': type,
+            'csrfmiddlewaretoken': '{{ csrf_token }}',
+            'contact_info': value
+          },
+          success: function(response) {
+            if(response && response["type"]) {
+              notification.show(response["type"], response["message"]);
+            }  else {
+              notification.show('success','Контакт добавлен' );
+            }
+            location.reload();
+          }
+        });
+        $("#contacts").append($new_contact);
+        popup.hide();
         });
       });
     });
